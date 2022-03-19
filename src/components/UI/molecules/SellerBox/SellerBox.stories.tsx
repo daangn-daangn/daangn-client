91e5eba3bc5f61e435b2dc2414054ebd06f66164
1998YUKI0331 <dkenl0110@dongguk.edu>
2022-03-20 01:04:28 +0900

    Feat #13 - Create Molecules/SellerBox

---
+++ src/components/UI/molecules/SellerBox/SellerBox.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, Story } from "@storybook/react";
+import SellerBox, { SellerBoxProps } from "./SellerBox";
+
+export default {
+  title: 'Molecules/SellerBox',
+  component: SellerBox,
+} as Meta;
+
+const Template: Story<SellerBoxProps> = (args) => (
+  <>
+    <SellerBox {...args} />
+  </>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  sellerName: "유키링",
+  sellerImgUrl: "https://img1.cgtrader.com/items/3095532/6fb947cfc0/large/hello-kitty-sanrio-3d-model-low-poly-obj-ztl.jpg",
+  location: "하라주쿠",
+};
---
+++ src/components/UI/molecules/SellerBox/SellerBoxStyled.tsx
@@ -0,0 +1,29 @@
+import styled from "@emotion/styled";
+import { SellerBoxProps } from "./SellerBox";
+
+export const SellerBoxStyled = styled.div<SellerBoxProps>`
+  width: 100%;
+  display: flex;
+  padding: 10px;
+  overflow-x: hidden;
+  border-bottom: 1px solid ${(props) => props.theme.$black20};
+  .seller-inform {
+    margin-left: 8px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    .seller-inform_name {
+      color: ${(props) => props.theme.$black};
+      font-weight: 700;
+      font-size: 1rem;
+    }
+    .seller-inform_location {
+      color: ${(props) => props.theme.$black80};
+      font-weight: 300;
+      font-size: 0.7rem;
+    }
+  }
+  .seller-temperature {
+    margin-left: auto;
+  }
+`;
