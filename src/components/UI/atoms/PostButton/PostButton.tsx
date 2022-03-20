0803ce3a84c19917e348d4410aeda23d4304a3b7
1998YUKI0331 <dkenl0110@dongguk.edu>
2022-03-20 14:28:56 +0900

    Feat #17 - Create atoms/PostButton

---
+++ src/components/UI/atoms/PostButton/PostButton.tsx
@@ -0,0 +1,35 @@
+import { useState, useRef } from 'react';
+import useOutsideClick from '../../../../hooks/useOutsideClick';
+import { PostButtonStyled, ModalStyled } from './PostButtonStyled';
+
+export interface PostButtonProps {
+  toggle?: boolean;
+}
+
+const PostButton = (props: PostButtonProps) => {
+  const [toggle, setToggle] = useState<boolean>(false);
+  const ModalOut = useRef() as React.MutableRefObject<HTMLDivElement>;
+  useOutsideClick(ModalOut, setToggle);
+
+  return (
+    <>
+      {toggle ? 
+        <ModalStyled />
+      :null}
+      <PostButtonStyled toggle={toggle} ref={ModalOut}>
+        {toggle ? 
+          <div className="modal-box">
+            <div className="Item">
+              🔖 내 물건 팔기
+            </div>
+          </div>
+        :null}
+        <button onClick={e => setToggle(!toggle)}>
+          {toggle ? "x" : "+"}
+        </button>
+      </PostButtonStyled>    
+    </>
+  );
+};
+
+export default PostButton;
