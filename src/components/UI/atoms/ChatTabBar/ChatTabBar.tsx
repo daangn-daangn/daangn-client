829982f402a4a9621e5ed452cb5d96e2f7c0dfdd
1998YUKI0331 <dkenl0110@dongguk.edu>
2022-04-04 15:43:37 +0900

    Temperature 수정 및 ChatTabBar 생성

---
+++ src/components/UI/atoms/ChatTabBar/ChatTabBar.tsx
@@ -0,0 +1,27 @@
+import { ChatTabBarStyled } from './ChatTabBarStyled';
+import { IUser } from 'interfaces/User.interface';
+import Temperature from '@atoms/Temperature/Temperature';
+import { ReactComponent as Back } from 'assets/back.svg';
+import { ReactComponent as Hamburger } from 'assets/hamburger.svg';
+
+
+export interface ChatTabBarProps {
+  userDetail: Pick<IUser, 'nickname' | 'manner'>;
+}
+
+const ChatTabBar = ({ userDetail }: ChatTabBarProps) => {
+  return (
+    <>
+      <ChatTabBarStyled {...userDetail}>
+        <Back width='18' height='18' />
+        <div className='user-detail'>
+          <span>{userDetail.nickname}</span>
+          <Temperature type="chat" degree={userDetail.manner}/>
+        </div>
+        <Hamburger width='18' height='18' />
+      </ChatTabBarStyled>
+    </>
+  );
+};
+
+export default ChatTabBar;
