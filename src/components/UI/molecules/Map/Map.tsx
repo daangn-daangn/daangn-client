80cd23440566370700655a22418140941d6b92c4
1998YUKI0331 <dkenl0110@dongguk.edu>
2022-04-18 00:50:30 +0900

    Feat #47 - Create Molecules/Map

---
+++ src/components/UI/molecules/Map/Map.tsx
@@ -0,0 +1,59 @@
+import { MapStyled } from './MapStyled';
+
+export interface MapProps {
+  latitude: number; //위도
+  longitude: number; //경도
+}
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+export default function Map(props: MapProps) {
+  const mapScript = document.createElement('script');
+
+  mapScript.async = true;
+  mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_API_KEY}&autoload=false`;
+
+  document.head.appendChild(mapScript);
+
+  const onLoadKakaoMap = () => {
+    window.kakao.maps.load(() => {
+      const mapContainer = document.getElementById('map');
+      const mapOption = {
+        center: new window.kakao.maps.LatLng(props.latitude, props.longitude), // 지도의 중심좌표
+        level: 3, // 지도의 확대 레벨
+      };
+      const map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+      const imageSrc = 'https://www.daangn.com/logo.png';
+      const imageSize = new window.kakao.maps.Size(60, 60);
+      const imageOption = { offset: new window.kakao.maps.Point(27, 69) };
+
+      const marker = new window.kakao.maps.Marker({
+        position: map.getCenter(),
+        image: new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+      });
+      marker.setMap(map);
+
+      window.kakao.maps.event.addListener(map, 'click', function (mouseEvent: any) {
+        const latlng = mouseEvent.latLng;
+        marker.setPosition(latlng);
+
+        let message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
+        message += '경도는 ' + latlng.getLng() + ' 입니다';
+
+        console.log(message);
+      });
+    });
+  };
+  mapScript.addEventListener('load', onLoadKakaoMap);
+
+  return (
+    <>
+      <MapStyled id="map" />
+    </>
+  );
+}
