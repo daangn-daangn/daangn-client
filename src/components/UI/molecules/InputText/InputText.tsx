3a26adfdb11ea1911b2761c6c424c19426af38f2
1998YUKI0331 <dkenl0110@dongguk.edu>
2022-05-26 23:10:43 +0900

    Feat #63 - Form 컴포넌트 InputText로 대체

--- src/components/UI/molecules/InputText/InputText.tsx
+++ src/components/UI/molecules/InputText/InputText.tsx
@@ -1,13 +1,17 @@
 import Input from '@atoms/Input/Input';
 import Label from '@atoms/Label/Label';
+import Message from '@atoms/Message/Message';
+import { UseFormRegisterReturn } from 'react-hook-form';
 import { InputTextStyled } from './InputTextStyled';
 
 export interface InputTextProps {
   label: string;
+  message?: string;
   placeholder?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   value?: string;
   inputRef?: React.RefObject<HTMLInputElement>;
+  register?: UseFormRegisterReturn;
 }
 
 const InputText = ({ inputRef, ...props }: InputTextProps) => {
@@ -15,7 +19,8 @@ const InputText = ({ inputRef, ...props }: InputTextProps) => {
     <>
       <InputTextStyled>
         <Label {...props} />
-        <Input ref={inputRef} {...props} />
+        <Input ref={inputRef} {...props} {...props.register} />
+        <Message {...props} />
       </InputTextStyled>
     </>
   );
