a07b305f5414d5235219d1d5aaab1ee06019c2f8
1998YUKI0331 <dkenl0110@dongguk.edu>
2022-05-29 00:03:02 +0900

    Feat #65 - Modify style, props of Top

--- src/components/UI/molecules/Top/Top.tsx
+++ src/components/UI/molecules/Top/Top.tsx
@@ -2,30 +2,33 @@ import Title from '@atoms/Title/Title';
 import { useCallback } from 'react';
 import { useNavigate } from 'react-router';
 import { StyledTop } from './TopStyled';
+import { ReactComponent as Back } from 'assets/back.svg';
+import { ReactComponent as Clsoe } from 'assets/close.svg';
 
+export type TopLeftType = 'none' | 'prev' | 'close';
 export interface TopProps {
-  isPrevArrow: boolean;
+  left?: TopLeftType;
+  leftClick?: (...args: any[]) => any | undefined;
   title: string;
   mb?: string;
+  right?: React.ReactNode;
 }
 
-const Top = ({ isPrevArrow, title, mb }: TopProps) => {
+const Top = ({ left, leftClick, title, right, mb }: TopProps) => {
   const navigate = useNavigate();
   const onClickArrow = useCallback(() => {
     navigate(-1);
+    if (leftClick !== undefined) leftClick();
   }, []);
   return (
-    <StyledTop isPrevArrow={isPrevArrow} mb={mb}>
-      {isPrevArrow && (
-        <a onClick={onClickArrow}>
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M11.67 3.87001L9.9 2.10001L0 12L9.9 21.9L11.67 20.13L3.54 12L11.67 3.87001Z" fill="#434343" />
-          </svg>
-        </a>
-      )}
-      <Title fontWeigt="400" fontSize="16px">
+    <StyledTop left={left || 'none'} mb={mb}>
+      <a onClick={onClickArrow}>
+        <svg>{left === 'prev' ? <Back /> : left === 'close' ? <Clsoe /> : null}</svg>
+      </a>
+      <Title fontWeigt="700" fontSize="16px">
         {title}
       </Title>
+      <div>{right}</div>
     </StyledTop>
   );
 };
--- src/components/UI/molecules/Top/TopStyled.tsx
+++ src/components/UI/molecules/Top/TopStyled.tsx
@@ -1,34 +1,37 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { TopLeftType } from './Top';
 
-export const StyledTop = styled.header<{ isPrevArrow: boolean; mb?: string }>`
+export const StyledTop = styled.header<{ left: TopLeftType; mb?: string }>`
   ${(props) => {
-    if (props.isPrevArrow) {
-      return css`
-        width: 100%;
-        padding: 12px 0;
-        margin-bottom: ${props.mb};
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        a {
-          cursor: pointer;
-        }
-        & > h3 {
-          flex: 1;
-          margin-right: 24px;
-          display: flex;
-          justify-content: center;
-        }
-      `;
-    }
     return css`
+      position: fixed;
+      left: 0;
+      top: 0;
       width: 100%;
-      padding: 12px 0;
+      padding: 12px 20px;
       margin-bottom: ${props.mb};
       display: flex;
       align-items: center;
-      justify-content: center;
+      justify-content: space-between;
+      background: white;
+      border-bottom: 1px solid ${props.theme.$black20};
+      a {
+        cursor: pointer;
+        svg {
+          width: 24px;
+          height: 24px;
+        }
+      }
+      & > h3 {
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
     `;
   }}
 `;
