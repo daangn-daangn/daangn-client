c1101b7837c3858230c9f260d75f261f4b48b230
LeeJaeHoon <dlwogns3413@naver.com>
2022-07-07 23:48:02 +0900

    Feat #cors-test - URL 변경

--- src/pages/CorsTest.tsx
+++ src/pages/CorsTest.tsx
@@ -6,12 +6,12 @@ const CorsTestPage = () => {
     axios
       .get('http://localhost:8080/api/health')
       .then((res) => console.log(res.data))
-      .catch((err) => console.log(err));
+      .catch((err) => console.log('/api/health ERROR: ', err));
 
     axios
-      .get('http://localhost:8080/chat/health')
+      .get('http://localhost:8880/chat/health')
       .then((res) => console.log(res.data))
-      .catch((err) => console.log(err));
+      .catch((err) => console.log('/chat/health ERROR: ', err));
   }, []);
   return <div></div>;
 };
