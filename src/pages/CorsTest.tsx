3d790687e8acdb6e038ff8a87c17dbacb3139bfb
zhtcks <75410527+cotchan@users.noreply.github.com>
2022-07-21 14:58:16 +0900

    Feat #91 - 배포설정에 env 주입 옵션 추가

--- src/pages/CorsTest.tsx
+++ src/pages/CorsTest.tsx
@@ -5,12 +5,18 @@ const CorsTestPage = () => {
   useEffect(() => {
     axios
       .get('/api/health')
-      .then((res) => console.log(res.data))
+      .then((res) => {
+        console.log(res.data);
+        console.log(`${process.env.REACT_APP_TEST_VARIABLE}`);
+      })
       .catch((err) => console.log('/api/health ERROR: ', err));
 
     axios
       .get('/chat/health')
-      .then((res) => console.log(res.data))
+      .then((res) => {
+        console.log(res.data);
+        console.log(`${process.env.REACT_APP_TEST_VARIABLE}`);
+      })
       .catch((err) => console.log('/chat/health ERROR: ', err));
   }, []);
   return <div></div>;
