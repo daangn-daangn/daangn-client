c26897af8a575a711f4246ce70617959eab595e0
LeeJaeHoon <dlwogns3413@naver.com>
2022-07-27 20:48:08 +0900

    Fix #112 - ProductDetailPage StateChangeButton Error edit

--- src/components/UI/organisms/ProductStateChangeButton/ProductStateChangeButton.tsx
+++ src/components/UI/organisms/ProductStateChangeButton/ProductStateChangeButton.tsx
@@ -24,7 +24,6 @@ const ProductStateChangeButton = ({ productId, productState }: ProductStateChang
   const queryKey = [QUERY_KEYS.PRODUCTS, productId];
   const mutation = useProductEditState({
     onMutate: async ({ productState }) => {
-      throw new Error();
       await queryClient.cancelQueries(queryKey);
       const previousProduct = queryClient.getQueryData<IProductWithUser>(queryKey);
       queryClient.setQueryData<IProductWithUser | undefined>(
