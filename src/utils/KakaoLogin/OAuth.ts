e339985072b592d1b42e3064f4f4e5dd912f01b5
zhtcks <75410527+cotchan@users.noreply.github.com>
2022-07-27 12:33:02 +0900

    Feat #112 - kakao login api redirect uri 경로 수정

--- src/utils/KakaoLogin/OAuth.ts
+++ src/utils/KakaoLogin/OAuth.ts
@@ -1,4 +1,4 @@
 export const CLIENT_ID = `${process.env.REACT_APP_KAKAO_API_KEY}`;
-export const REDIRECT_URI = `http://localhost:3000/oauth/callback/kakao`;
+export const REDIRECT_URI = `${process.env.REACT_APP_REDIRECT_URI}`;
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
